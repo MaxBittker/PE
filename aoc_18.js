d65d41c5f51e1a388c5c33e8087cf29a82f77cb4
maxbittker <maxbittker@gmail.com>
2017-12-23 18:32:19 -0800

    aoc 18 part 2

--- aoc_18.js
+++ aoc_18.js
@@ -1,61 +1,6 @@
 isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
 
-registers = {};
-last = null;
-PC = 0;
-exited = false;
-
-resolve = v => {
-  if (isNumeric(v)) {
-    return parseInt(v, 10);
-  } else {
-    return registers[v] || 0;
-  }
-};
-
-// snd X plays a sound with a frequency equal to the value of X.
-snd = x => last = resolve(x);
-// set X Y sets register X to the value of Y.
-set = (x, y) => registers[x] = y;
-// add X Y increases register X by the value of Y.
-add = (x, y) => registers[x] = resolve(x) + y;
-// mul X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
-mul = (x, y) => registers[x] = resolve(x) * y;
-// mod X Y sets register X to the remainder of dividing the value contained in register X by the value of Y (that is, it sets X to the result of X modulo Y)
-mod = (x, y) => registers[x] = resolve(x) % y;
-// rcv X recovers the frequency of the last sound played, but only when the value of X is not zero. (If it is zero, the command does nothing.)
-rcv = x => {
-  if (resolve(x) !== 0) {
-    console.log("recovered" + last);
-    exited = true;
-  }
-};
-// jgz X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero. (An offset of 2 skips the next instruction, an of
-jgz = (x, y) => {
-  if (resolve(x) > 0) {
-    PC += y - 1;
-  }
-};
-
-call = str => {
-  [fn, x, y] = str.split(" ");
-  operations[fn](x, resolve(y));
-};
-
-let operations = { snd, set, add, mul, mod, rcv, jgz };
-
-let input = `set a 1
-add a 2
-mul a a
-mod a 5
-snd a
-set a 0
-rcv a
-jgz a -1
-set a 1
-jgz a -2`;
-
-input = `set i 31
+let lines = `set i 31
 set a 1
 mul p 17
 jgz p p
@@ -95,18 +40,95 @@ add i -1
 jgz i -11
 snd a
 jgz f -16
-jgz a -19`
-lines = input.split("\n");
+jgz a -19`.split("\n");
 
-while (!exited) {
-    toRun = lines[PC]
-    console.log("running " + toRun)
-    call(toRun)
-    console.log(registers)
-    
-    if(exited){
-        console.log(last)
-       break; 
+
+class Computer {
+  constructor(n, friend) {
+    this.registers = {};
+    this.registers["p"] = n;
+    this.queue = [];
+    this.friend = friend;
+    this.last = null;
+    this.PC = 0;
+    this.nSent = 0;
+  }
+  run() {
+    if (this.PC > lines.length - 1) {
+      console.log("ran off");
+      return;
     }
-    PC++;
+    let toRun = lines[this.PC];
+    // console.log("running " + toRun);
+    // console.log(this.registers);
+
+    this.call(toRun);
+    this.PC++;
+  }
+  call(str) {
+    let [fn, x, y] = str.split(" ");
+    this[fn](x, this.resolve(y));
+  }
+  resolve(v) {
+    if (isNumeric(v)) {
+      return parseInt(v, 10);
+    } else {
+      return this.registers[v] || 0;
+    }
+  }
+
+  // snd X plays a sound with a frequency equal to the value of X.
+  snd(x) {
+    this.nSent++;
+    return this.friend.queue.push(this.resolve(x));
+  }
+  // set X Y sets register X to the value of Y.
+  set(x, y) {
+    return (this.registers[x] = y);
+  }
+  // add X Y increases register X by the value of Y.
+  add(x, y) {
+    return (this.registers[x] = this.resolve(x) + y);
+  }
+  // mul X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
+  mul(x, y) {
+    return (this.registers[x] = this.resolve(x) * y);
+  }
+  // mod X Y sets register X to the remainder of dividing the value contained in register X by the value of Y
+  mod(x, y) {
+    return (this.registers[x] = this.resolve(x) % y);
+  }
+  // rcv X recovers the frequency of the last sound played, but only when the value of X is not zero.
+  rcv(x) {
+    let nextValue = this.queue.shift();
+    if (nextValue == undefined) {
+      this.PC -= 1;
+    } else {
+      this.registers[x] = nextValue;
+    }
+  }
+  // jgz X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero. (An offset of 2 skips the next instruction, an of
+  jgz(x, y) {
+    if (this.resolve(x) > 0) {
+      this.PC += y - 1;
+    }
+  }
 }
+
+
+let A = new Computer(0);
+let B = new Computer(1, A);
+A.friend = B;
+
+while (true) {
+  let appc = A.PC;
+  let bppc = B.PC;
+  A.run();
+  B.run();
+  console.log(A.queue.length,B.queue.length)
+  if (appc == A.PC && bppc == B.PC) {
+    console.log("deadlock");
+    break;
+  }
+}
+console.log(B.nSent);
