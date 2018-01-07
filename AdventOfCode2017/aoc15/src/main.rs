0f102bbec00467af1fdea7bd674f3828807bd04e
maxbittker <maxbittker@gmail.com>
2018-01-06 16:36:28 -0800

    nice

--- AdventOfCode2017/aoc15/src/main.rs
+++ AdventOfCode2017/aoc15/src/main.rs
@@ -1,21 +1,36 @@
-//lowest 16 bits of both values, and keeps track of the number of times those parts of the values
-//match.
 
-//take the previous value it produced, multiply it by a factor (generator A uses 16807; generator
-//B uses 48271), and then keep the remainder of dividing that resulting product by 2147483647. That
-
-// fn generator_maker(factor: i64) -> (fn(i64) -> i64) {
-//     |prev| (prev * factor).wrapping_rem(2147483647);
-// }
-
-fn generator_maker(factor: i64) -> Box<Fn(i64) -> i64> {
-    Box::new(move |prev| (prev * factor).wrapping_rem(2147483647))
+fn generator_maker(factor: i64, multiple: i64) -> Box<Fn(i64) -> i64> {
+    Box::new(move |prev| {
+        let mut canditate = (prev * factor).wrapping_rem(2147483647);
+        loop {
+            if canditate % multiple == 0 {
+                return canditate;
+            } else {
+                canditate = (canditate * factor).wrapping_rem(2147483647);
+            }
+        }
+    })
 }
 
 
 fn main() {
-    let a = generator_maker(16807);
-    let b = generator_maker(48271);
+    let a = generator_maker(16807, 4);
+    let b = generator_maker(48271, 8);
+    let mut a_v = 634;
+    let mut b_v = 301;
+    let mut c = 0;
 
-    println!("{:?} : {:?}", a(65), b(65));
+    for _ in 0..5_000_000 {
+        a_v = a(a_v);
+        b_v = b(b_v);
+        // println!("{:b} \n{:b}\n", a_v % 65536, b_v % 65536);
+
+        if a_v % 65536 == b_v % 65536 {
+            c += 1;
+        }
+    }
+
+    println!("{}", c);
 }
+
+//
